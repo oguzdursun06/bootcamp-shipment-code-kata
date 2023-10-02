6982e78f4e085e03d6033f7741cd030283bfd195
Oğuzhan Dursun <oguzhan.dursun@siemens.com>
2023-10-02 10:46:19 +0300

    feat(basket): implemented initial solution to obtain the shipment size of the basket

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,13 +1,29 @@
 package com.trendyol.shipment;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Basket {
 
     private List<Product> products;
 
+    public static final int PRODUCT_SHIPMENT_THRESHOLD = 3;
+
     public ShipmentSize getShipmentSize() {
-        return null;
+        Map<ShipmentSize, Long> shipmentSizeFrequencyMap = products.stream()
+                .collect(Collectors.groupingBy(Product::getSize, Collectors.counting()));
+
+        return shipmentSizeFrequencyMap.entrySet()
+                .stream()
+                .filter(shipmentSizeFrequency -> shipmentSizeFrequency.getValue() >= PRODUCT_SHIPMENT_THRESHOLD)
+                .max(Map.Entry.comparingByKey())
+                .map(shipmentSizeFrequency ->
+                        ShipmentSize.getUpperShipmentSize(shipmentSizeFrequency.getKey()))
+                .orElse(getMaxValue());
+    }
+
+    private ShipmentSize getMaxValue() {
+        return Collections.max(products, Comparator.comparingInt(product -> product.getSize().ordinal())).getSize();
     }
 
     public List<Product> getProducts() {
--- src/main/java/com/trendyol/shipment/ShipmentSize.java
+++ src/main/java/com/trendyol/shipment/ShipmentSize.java
@@ -5,5 +5,12 @@ public enum ShipmentSize {
     SMALL,
     MEDIUM,
     LARGE,
-    X_LARGE
+    X_LARGE;
+
+    public static ShipmentSize getUpperShipmentSize(ShipmentSize shipmentSize) {
+        if(shipmentSize.equals(X_LARGE)){
+            return X_LARGE;
+        }
+        return ShipmentSize.values()[shipmentSize.ordinal() + 1];
+    }
 }
