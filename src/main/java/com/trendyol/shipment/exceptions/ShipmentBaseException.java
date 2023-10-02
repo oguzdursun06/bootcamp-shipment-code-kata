6306ad100c9c3a8823d3876d5ec9e29c3f2d544f
Oğuzhan Dursun <oguzhan.dursun@siemens.com>
2023-10-02 23:01:16 +0300

    refactor: added ShipmentBaseException and moved to exceptions package

---
+++ src/main/java/com/trendyol/shipment/exceptions/ShipmentBaseException.java
@@ -0,0 +1,8 @@
+package com.trendyol.shipment.exceptions;
+
+public class ShipmentBaseException extends RuntimeException {
+
+    public ShipmentBaseException(String message){
+        super(message);
+    }
+}
